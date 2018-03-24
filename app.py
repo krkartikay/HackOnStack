4cd0a1f14d6017eb543cbac48215e896a90167fb
Kumar Kartikay <krkartikay@gmail.com>
2018-03-24 10:56:34 +0530

    Render template code

--- app.py
+++ app.py
@@ -5,19 +5,28 @@ app.secret_key = 'random secret key'
 
 @app.route('/')
 def homepage():
-    pass
+    return render_template("index.html")
 
 @app.route('/login/', methods=['POST','GET'])
 def login():
-    pass
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        pass
 
 @app.route('/register/', methods=['POST','GET'])
 def register():
-    pass
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        pass
 
 @app.route('/post/question/', methods=['POST','GET'])
 def post_question():
-    pass
+    if request.method == "GET":
+        return render_template("ask_question.html")
+    else:
+        pass
 
 @app.route('/post/answer/', methods=['POST'])
 def post_answer():
@@ -25,7 +34,7 @@ def post_answer():
 
 @app.route('/question/<int:q_id>')
 def question(q_id):
-    pass
+    return render_template("display_question.html")
 
 @app.route('/upvote/question/<int:q_id>/', methods=['POST'])
 def upvote_q(q_id):
@@ -45,7 +54,7 @@ def downvote_q(a_id):
 
 @app.route('/profile/<int:u_id>/')
 def function(u_id):
-    pass
+    return render_template("profile.html")
 
 if __name__ == '__main__':
     app.debug = True
