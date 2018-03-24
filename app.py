604b96ec629e8aea64bb884f265051855213b184
Kumar Kartikay <krkartikay@gmail.com>
2018-03-24 12:05:34 +0530

    made some models

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
 from flask import *
+from flask_sqlalchemy import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:qwerty@localhost/hackonstack'
+db = SQLAlchemy(app)
 app.secret_key = 'random secret key'
+
+#### MODELS
+
+class User(db.Model):
+    __tablename__ = "users"
+    u_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phoneno = db.Column(db.String(13))
+    bio = db.Column(db.String(1000))
+    dob = db.Column(db.DateTime, nullable=False)
+
+class Question(db.Model):
+    __tablename__ = "questions"
+    q_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text)
+    author_u_id = db.Column(db.Integer, nullable=False)
+    post_time = db.Column(db.DateTime, nullable=False)
+    upvotes = db.Column(db.Integer)
+    # upvoters_id = ??
+
+class Answers(db.Model):
+    __tablename__ = "answers"
+    a_id = db.Column(db.Integer, primary_key=True, nullable=False)
+    q_id = db.Column(db.Integer, nullable=False)
+    body = db.Column(db.Text)
+    author_u_id = db.Column(db.Integer)
+    post_time = db.Column(db.DateTime)
+    upvotes = db.Column(db.Integer)
+
+db.create_all()
+
+#### ROUTES
 
 @app.route('/')
 def homepage():
